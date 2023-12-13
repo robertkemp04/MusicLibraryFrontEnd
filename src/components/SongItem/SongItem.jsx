c3fb215e98927cb92548877dd96a8fece69949d5
Robert Kemp <robert.kemp04@outlook.com>
2023-12-12 19:01:44 -0600

    Interactive favorite button complete

--- src/components/SongItem/SongItem.jsx
+++ src/components/SongItem/SongItem.jsx
@@ -1,12 +1,23 @@
+import React, { useState } from "react";
+import "./SongItem.css";
+
 const SongItem = ({ title }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  console.log(isFavorite);
+
   const handleClick = (e) => {
-    console.log(e);
+    setIsFavorite(!isFavorite);
   };
+
+  const btnClass = isFavorite ? "active-btn" : "";
 
   return (
     <div className="movie-item">
       <span>{title}</span>
-      <button onClick={handleClick}>Favorite</button>
+      <button className={btnClass} onClick={handleClick}>
+        Favorite
+      </button>
     </div>
   );
 };
