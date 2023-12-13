b21fe3febd56a9ce07828e0b24cd660826b200de
Robert Kemp <robert.kemp04@outlook.com>
2023-12-13 09:49:40 -0600

    Dummy data created

---
+++ src/data/data.js
@@ -0,0 +1,32 @@
+const initData = [
+  {
+    title: "Righteous",
+    album: "Legends Never Die",
+    artist: "Juice WRLD",
+    genre: "Hip-Hop",
+    releaseDate: "April 23 2020",
+  },
+  {
+    title: "Married Next Year",
+    album: "Beautiful Mind",
+    artist: "Rod Wave",
+    genre: "Hip-Hop",
+    releaseDate: "August 11 2022",
+  },
+  {
+    title: "White Iverson",
+    album: "Stoney",
+    artist: "Post Malone",
+    genre: "Hip-Hop",
+    releaseDate: "July 19 2015",
+  },
+  {
+    title: "Go",
+    album: "F*ck Love",
+    artist: "The Kid Laroi",
+    genre: "Hip-Hop",
+    releaseDate: "June 11 2020",
+  },
+];
+
+export default initData;
